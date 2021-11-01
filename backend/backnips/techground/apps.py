238447b3764729700d24288ef8bfbadab472c1eb
huhji <huhji.elha@gmail.com>
2021-11-01 15:06:40 +0900

    delete django project

--- backend/backnips/techground/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TechgroundConfig(AppConfig):
-    name = 'techground'
--- backend/backnips/techground/views.py
+++
@@ -1,6 +0,0 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render, HttpResponse
-
-
-def index(request):
-    return HttpResponse("Hello Kakao Nips! This is Tech Ground.")
